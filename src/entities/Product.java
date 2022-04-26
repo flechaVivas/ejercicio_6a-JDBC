75dd19729ab552551ac376f5755ee0bb8f964b0c
flechaVivas <facundovivas15@gmail.com>
2022-04-25 22:50:49 -0300

    Agrego archivos

---
+++ src/entities/Product.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class Product {
+	
+	private int id;
+	private String name; 	
+	private String description;
+	private double price;
+	private int stock;
+	private boolean shippingIncluded;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public boolean isShippingIncluded() {
+		return shippingIncluded;
+	}
+	public void setShippingIncluded(boolean shippingIncluded) {
+		this.shippingIncluded = shippingIncluded;
+	}
+	
+	
+	public Product() {};
+	
+	
+}
